305d586eff17fbd8880e30a6be9508ef07454766
Lewis Benson <tm.lewisbenson@gmail.com>
2025-04-17 21:02:33 -0400

    refactor variable

--- src/getRandomPokemon.js
+++ src/getRandomPokemon.js
@@ -6,7 +6,6 @@ import { randInt } from "./randInt.js";
 export async function getRandomPokemon() {
   const id = randInt(151) + 1;
   const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
-  const data = await res.json();
-  const { name, height, weight } = data;
+  const { name, height, weight } = await res.json();
   return { id, name: capitalize(name), height, weight };
 }
